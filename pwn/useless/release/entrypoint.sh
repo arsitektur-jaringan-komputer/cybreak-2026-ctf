ddd25803c71f82d67219d2b912416519dd20fd8b
unknown <mnabil190405@gmail.com>
2026-09-12 12:57:06 +0700

    fix(pwn/useless): sync release source and runtime configuration

--- pwn/useless/release/entrypoint.sh
+++ pwn/useless/release/entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 set -eu
+: "${RCTF_FLAG:?RCTF_FLAG must be supplied by the instancer}"
+flag_name="$(od -An -N16 -tx1 /dev/urandom | tr -d ' \n')"
+flag_path="/run/ctf/$flag_name"
+printf '%s\n' "$RCTF_FLAG" > "$flag_path"
+chmod 0444 "$flag_path"
+unset RCTF_FLAG RCTF_FLAGS
 exec socat TCP-LISTEN:1337,reuseaddr,fork EXEC:/home/mirai/chall,stderr,su=mirai
